c5a5d302454c0613b7ea4bfb6fad38b8840cce6a
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-24 13:36:26 +0700

    if-else statement

--- index.php
+++ index.php
@@ -1,46 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <!-- method="get" will show the var on the url  -->
-  <form action="index.php" method="post">
-    <label>x:</label>
-    <input type=" text" name="x"><br>
-    <label>y:</label>
-    <input type=" text" name="y"><br>
-    <label>z:</label>
-    <input type=" text" name="z"><br>
-    <input type="submit" value="total">
-  </form>
-</body>
-
-</html>
-
 <?php
-	$x = $_POST["x"];
-	$y = $_POST["y"];
-	$z = $_POST["z"];
-	$total = null;
-
-	// $total = abs($x);
-	// $total = round($x,2);
-	// $total = floor($x);
-	// $total = ceil($x);
-	// $total = pow($x, $y);
-	// $total = sqrt($x);
-	// $total = max($x, $y, $z);
-	// $total = min($x, $y, $z);
-	// $total = pi();
-	$total = rand(1, 6);
 	
-	
+	$age = 17;
+	if($age >=18){
+		echo "You may entere this site";
+	}
+	elseif($age <= 0){
+		echo "That wasn't a valid age.";
+	} 
+	else{
+		echo "You must be 18+ to enter";
+	}
 
-	echo $total
 	
 ?>
