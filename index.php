d9660160b13b13a7a8df30f7bec0acc96eecd3d0
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-25 14:17:47 +0700

    isset and empty function

--- index.php
+++ index.php
@@ -1,39 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="index.php" method="post">
+    <label>username: </label>
+    <input type="text" name="username"><br>
+    <label>password: </label>
+    <input type="password" name="password"><br>
+    <input type="submit" name="login" value="Login">
+  </form>
+</body>
+
+</html>
+
 <?php
 
-  // also use the array method
+// isset() = Returns TRUE if a variable is declared and not null
+// empty() = Return TRUE if a variable is not declared, false, null, ""
 
-  $capitals = array("USA"=>"Washington D.C", 
-                    "Japan"=>"Kyoto", 
-                    "South Korea"=>"Seoul", 
-                    "India"=>"New Delhi");
-  
-  // echo $capitals["USA"];
-  $capitals["USA"] = "Las Vegas";
-  $capitals["China"] = "Beijing";
+// $username =  "Vatana";
 
-  foreach ($capitals as $key => $value) {
-    echo "{$key} = {$value}<br>";
+// echo isset($username) . "<br>"; // return 1 or "nothing"
+
+// if(isset($username)){
+//   echo "This variable is set<br>";
+// }
+// else {
+//   echo "This variable is NOT set<br>";
+// }
+
+// if(empty($username)){
+//   echo "This variable is empty<br>";
+// }
+// else {
+//   echo "This variable is NOT empty<br>";
+// }
+
+
+foreach ($_POST as $key => $value) {
+  echo "{$key} = {$value} <br>";
+}
+
+echo "<br";
+
+if (isset($_POST["login"])) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  if (empty($username)) {
+    echo "Username is missing";
+  } elseif (empty($password)) {
+    echo "Password is missing";
+  } else {
+    echo "Hello {$username}";
   }
 
-  echo "<br>";
+}
 
-  $keys = array_keys($capitals);
-  foreach ($keys as $key){
-    echo "{$key} <br>";
-  }
-
-  echo "<br>";
-
-  $values = array_values($capitals);
-  foreach ($values as $value){
-    echo "{$value} <br>";
-  }
-
-  echo "<br>";
-
-  $capitals = array_flip($capitals);
-  foreach ($capitals as $key => $value) {
-    echo "{$key} = {$value}<br>";
-  }
-  
 ?>
