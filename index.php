d65573c5bb3b8972c2bcded3274f2efad33cf1e5
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-26 11:31:32 +0700

    function

--- index.php
+++ index.php
@@ -1,31 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <form action="index.php" method="post">
-    <input type="checkbox" name="foods[]" value="Pizza">Pizza<br>
-    <input type="checkbox" name="foods[]" value="Hamburger">Hamburger<br>
-    <input type="checkbox" name="foods[]" value="Hotdog">Hotdog<br>
-    <input type="checkbox" name="foods[]" value="Taco">Taco<br>
-
-    <input type="submit" name="submit">
-  </form>
-</body>
-
-</html>
-
 <?php
-if (isset($_POST["submit"])) {
-  $foods = $_POST["foods"];
 
-  foreach ($foods as $food) {
-    echo $food . "<br>";
-  }
+// datatype is optional to add.
+function addNum(int $a, $b)
+{
+  return $a + $b;
 }
+
+echo addNum(1, 2);
+
 ?>
