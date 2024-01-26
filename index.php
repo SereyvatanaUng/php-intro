ccc738221a67234b68c08978798d55674ba4d43d
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-26 11:46:02 +0700

    some string function

--- index.php
+++ index.php
@@ -1,11 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Hello</h1>
+</body>
+
+</html>
+
 <?php
 
-// datatype is optional to add.
-function addNum(int $a, $b)
-{
-  return $a + $b;
-}
+$username = "Serey Vatana";
+$phone = '123-456-789';
 
-echo addNum(1, 2);
+// $username = strtolower($username);
+// $username = strtoupper($username);
+// $username = trim($username);
+// $phone = str_replace('-', '', $phone);
+// $username = strrev($username);
+// $username = str_shuffle($username);
+
+// // match 0, not match = 1
+// $equals = strcmp($username, "Serey Vatana");
+// echo $equals . "<br>";
+
+// $count = strlen($phone);
+
+// $index = strpos($phone, "-");
+
+// $firstname = substr($username, 0, 3);
+// $lastname = substr($username, 4);
+
+// // split a string into array
+// $fullname = explode(' ', $username);
+// foreach($fullname as $name){
+//   echo $name . "<br>";
+// }
+
+// // join an array to a string
+// $name2 = array("Ung", "Serey", "Vatana");
+// $name2 = implode(" ", $name2);
+// echo $name2 . "<br>";
+
+echo $username . "<br>";
+echo $phone;
 
 ?>
